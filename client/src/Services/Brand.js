f972dd4bd7dc25cbef329592bce51ae7c648458d
Akanksha Dodamani <akankshadodamani20@gmail.com>
2026-08-01 15:40:10 +0530

    update API URLs in Brand and Product services to use BASE_URL

--- client/src/Services/Brand.js
+++ client/src/Services/Brand.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 export const getAllBrands = async () => {
   return await axios({
     method: "GET",
-    url: `/api/brand/getAllBrands`,
+    url: `${BASE_URL}/api/brand/getAllBrands`,
   });
 };
 
 export const getBrandBySlug = async (brandSlug) => {
   return await axios({
     method: "GET",
-    url: `/api/brand/getBrandBySlug`,
+    url: `${BASE_URL}/api/brand/getBrandBySlug`,
     params: {
       slug: brandSlug
     }
--- client/src/Services/Product.js
+++ client/src/Services/Product.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+
 export const getAllProductsByBrand = async (brandSlug) => {
   return await axios({
     method: "GET",
-    url: `/api/product/getAllProductsByBrand`,
+    url: `${BASE_URL}/api/product/getAllProductsByBrand`,
     params: {
       slug: brandSlug,
     },
